e82ac72bab476342e778b0cb8407df333067178d
liaokongVFX <568250549@qq.com>
2021-08-25 22:55:01 +0800

    添加 get_events 接口，数据还是mock数据

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from views.users import users_bp
+from views.events import events_bp
 
 app = Flask(__name__)
 app.register_blueprint(users_bp)
+app.register_blueprint(events_bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
--- views/events.py
+++ views/events.py
@@ -1,3 +1,36 @@
 # -*- coding: utf-8 -*-
+from flask import Blueprint, request, jsonify
+
+events_bp = Blueprint('events', __name__)
 
 
+@events_bp.route('/api/events/get_events', methods=['GET'])
+def get_events():
+    # fixme 从数据库中请求数据
+    return jsonify({
+        'status': 'success',
+        'msg': 'request successful',
+        'data': [
+            {
+                'id': 1,
+                'group': '事件组1',
+                'status': 'run',
+                'run_events': 6,
+                'error_num': 30
+            },
+            {
+                'id': 2,
+                'group': '事件组2',
+                'status': 'stop',
+                'run_events': 12,
+                'error_num': 66
+            },
+            {
+                'id': 3,
+                'group': '事件组1',
+                'status': 'run',
+                'run_events': 3,
+                'error_num': 0
+            },
+        ]
+    })
