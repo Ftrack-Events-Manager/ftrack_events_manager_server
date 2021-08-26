f2b998ee71b2d6feb423e9e220ada5d5dcd5a6c7
liaokongVFX <568250549@qq.com>
2021-08-26 23:18:19 +0800

    get_groups 接口改为从数据库中获取数据 封装了mongodb的库

---
+++ views/groups.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, request, jsonify
+from .utils.db_helper import DBInfo, DBLog
+
+groups_bp = Blueprint('groups', __name__)
+
+
+@groups_bp.route('/api/groups/get_groups', methods=['GET'])
+def get_events():
+    groups = DBInfo.get_groups()
+    data = []
+    for group in groups:
+        error_num = len(DBLog.get_errors(group['name']))
+        run_events = 0
+        for event_id in group['events']:
+            event = DBInfo.get_event_by_id(event_id)
+            if event.get('enabled'):
+                run_events += 1
+
+        data.append({
+            'id': group['id'],
+            'name': group['name'],
+            'run_events': run_events,
+            'error_num': error_num
+        })
+
+    data.sort(key=lambda x: x['name'])
+    print(data)
+
+    return jsonify({
+        'status': 'success',
+        'msg': 'request successful',
+        'data': data
+    })
---
+++ views/utils/db_helper.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@author: LiaoKong
+@time: 2021/08/26 22:00
+"""
+from .mongo import Mongo
+
+DB_INFO = ('FtrackEventsManager', 'EventsInfos')
+DB_LOG = ('FtrackEventsManager', 'EventsLogs')
+
+
+class DBInfo(object):
+    db = Mongo(*DB_INFO)
+
+    @classmethod
+    def get_groups(cls):
+        return cls.db.query({'type': 'group'}).all(True)
+
+    @classmethod
+    def get_event_by_id(cls, event_id):
+        return cls.db.query_one({'type': 'event', 'id': event_id}) or {}
+
+
+class DBLog(object):
+    db = Mongo(*DB_LOG)
+
+    @classmethod
+    def get_errors(cls, group_name):
+        return cls.db.query({'type': {'$ne': 'info'}, 'group': group_name}).all(True)
